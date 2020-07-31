16800d37ae259225ab220b831c5426be2ee5702c
Randhir <randhirpct2000@gmail.com>
2020-08-01 05:12:44 +0530

    App-Notes

---
+++ app/src/main/java/com/example/randhir/notes/NoteActivity.java
@@ -0,0 +1,63 @@
+package com.example.randhir.notes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.HashSet;
+
+public class NoteActivity extends AppCompatActivity {
+
+    EditText editText ;
+
+     int noteID ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+        editText = findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+
+        noteID = intent.getIntExtra("noteID",-1);
+
+        if(noteID != -1){
+            editText.setText(MainActivity.notes.get(noteID));
+        }else {
+            MainActivity.notes.add("");
+            noteID = MainActivity.notes.size() - 1 ;
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.notes.set(noteID,String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.randhir.notes", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+    }
+}
